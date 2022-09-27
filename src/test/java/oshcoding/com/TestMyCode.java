2eb222cefd3ff1fc7096cfc4f80beadd5c507be2
oshan-sameera <oshan951@gmail.com>
2022-09-27 13:55:13 +1000

    dlete testing file

--- src/test/java/oshcoding/com/TestMyCode.java
+++ src/test/java/oshcoding/com/TestMyCode.java
@@ -3,12 +3,19 @@ package oshcoding.com;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class TestMyCode {
 
+	public static Logger logger = LoggerFactory.getLogger(TestMyCode.class);
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void contextLoads() {
+		logger.info("Test case Executing...");
+		assertEquals(true, true);
 	}
 
 }
